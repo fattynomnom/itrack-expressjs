c4d3284d16b22967cb1db193c3e9dfded7d4cc76
Madeleine Cheng Mei Tee <madeleinetee@hotmail.com>
2023-11-08 18:15:43 +0800

    replace sql with knex

--- src/Resolvers.ts
+++ src/Resolvers.ts
@@ -1,18 +1,16 @@
-import pool from './database'
+import Knex from './database'
+import { User } from './models'
 
 const Resolvers = {
     Query: {
         getAllUsers: async () => {
-            const { rows } = await pool.query('SELECT * from users')
+            const rows = await Knex('users').select('*')
             return rows
         }
     },
     Mutation: {
-        addUser: async (_: unknown, { email }: { email: string }) => {
-            const { rows } = await pool.query(
-                'INSERT INTO users (email) VALUES ($1) RETURNING email',
-                [email]
-            )
+        addUser: async (_: unknown, user: User) => {
+            const rows = await Knex('users').insert(user, '*')
             return rows[0]
         }
     }
--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,17 @@
-import { Pool } from 'pg'
 import dotenv from 'dotenv'
+import knex from 'knex'
 
 dotenv.config()
 
-const pool = new Pool({
-    user: process.env.POSTGRESQL_USER,
-    password: process.env.POSTGRESQL_PW,
-    host: process.env.POSTGRESQL_HOST,
-    port: Number(process.env.POSTGRESQL_PORT),
-    database: process.env.POSTGRESQL_DB
+const Knex = knex({
+    client: 'pg',
+    connection: {
+        user: process.env.POSTGRESQL_USER,
+        password: process.env.POSTGRESQL_PW,
+        host: process.env.POSTGRESQL_HOST,
+        port: Number(process.env.POSTGRESQL_PORT),
+        database: process.env.POSTGRESQL_DB
+    }
 })
 
-export default pool
+export default Knex
